61c0c4ca29b277da0e10a669e2cd430d5115f0bf
Krutik Patel <kpatel43@students.towson.edu>
2024-10-05 01:39:00 -0400

    Created the basic website template to connect the frontend and the backend of the website.

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const { json } = require("body-parser");
+var express = require("express");
+var app = express();
+var cors = require("cors");
+app.use(cors());
+
+app.get("/login", function(request, response){
+    response.send("User Login");
+});
+
+app.listen(3001);
