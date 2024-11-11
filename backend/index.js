a577e1200f496f1b257d6b3d17223dc61cf15921
Deepak-DCB <binkamdeepak@gmail.com>
2024-11-10 23:49:06 -0500

    Started index.js

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,25 @@ var app = express();
 var cors = require("cors");
 app.use(cors());
 
+const UserModel = require('./models/User.js');
+const tableReservationModel = require('./models/TableReservation.js');
+const readingListModel = require('./models/ReadingList.js');
+const bookRentalModel = require('./models/BooksRental.js');
+const bookModel = require('./models/Book.js');
+
+
 app.get("/login", function(request, response){
     response.send("User Login");
+    const { email, password } = request.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+
+    })
+    .catch(error => {
+        
+    })
 });
+
+
 
 app.listen(3001);
