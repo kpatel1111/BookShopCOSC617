f107b363a7b7d601ab34e8c6884d53fd77a7d6a6
Krutik Patel <kpatel43@students.towson.edu>
2024-12-04 00:46:20 -0500

    updated the login file.

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,11 +9,10 @@ function Login({ setUserEmail }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
-    const apiUrl = "https://starlit-frangipane-ba4816.netlify.app";
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        axios.post("${apiUrl}/login", { email, password })
+        axios.post("http://localhost:3001/login", { email, password })
             .then(result => {
                 if (result.data === "Login Successful") {
                     alert("Login Successful");
